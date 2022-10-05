f5b4af5554a92becf0b8df39d4efbf0d69312052
Tom Porter <tom.port@yahoo.co.uk>
2022-10-05 10:59:38 +0100

    Promises and  callback/arrow/error handler tasks filled in

--- CbLocationCount.js
+++ CbLocationCount.js
@@ -8,12 +8,17 @@ const sitelistApiClient = new SitelistApiClient();
 sitelistApiClient.getLocations(
       // supply a function expecting one argument: locationList
       // the function should display the contents of the list
+      function (locations) {
+        console.log(`We have ${locations.length} locations.` );
+      }
 );
 
 // TODO:1 implement using arrow function
 sitelistApiClient.getLocations(
      // supply a function expecting one argument: locationList
      // the function should display the contents of the list
+     locations => 
+        console.log(`We have ${locations.length} locations.`)
 );
 
 
@@ -21,9 +26,12 @@ sitelistApiClient.getLocations(
 sitelistApiClient.getLocations(
     // supply a function expecting one argument: locationList
     // the function should display the contents of the list
-
+    locations => 
+        console.log(`We have ${locations.length} locations.`),
     // supply a second function, the error handler
     // test by amending getLocations() to fail
+    error =>
+        console.log(`Failure - ${error}`)
 );
 
 
--- PromisesLocationCount.js
+++ PromisesLocationCount.js
@@ -3,8 +3,13 @@ import SitelistApiClient from './apiClients/sitelistApiClient.js'
 const sitelistApiClient = new SitelistApiClient();
 
 
-let locationPromise = sitelistApiClient.getLocations();
 
 // TODO:2 use locationPromise.then() to display a count of the locations
 
 
+
+let locationPromise = sitelistApiClient.getLocations();
+locationPromise.then(
+    locationsList =>
+            console.log("We have " + locationsList.length + " locations")
+);
